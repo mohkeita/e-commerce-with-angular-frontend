995a312e0078c6f139a40b9623346c3fa2537b2b
Mohamed Keita <mohkeita13@gmail.com>
2020-11-28 23:48:26 +0100

    Build UI for shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {IProduct} from './models/product';
-import {IPagination} from './models/pagination';
+import {IProduct} from './shared/models/product';
+
 
 @Component({
   selector: 'app-root',
@@ -10,17 +9,11 @@ import {IPagination} from './models/pagination';
 })
 export class AppComponent {
   title = 'E-commerce';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50')
-      .subscribe((response:IPagination) => {
-        this.products = response.data;
-      }, error => {
-        console.log(error);
-      });
+
   }
 }
